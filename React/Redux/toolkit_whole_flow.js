d279f7c6660253c9def57f580b17ceba06bbaf9e
zhouyinglan.1 <zhouyinglan.1@jd.com>
2025-05-02 21:26:48 +0800

    1

--- React/Redux/toolkit_whole_flow.js
+++ React/Redux/toolkit_whole_flow.js
@@ -34,6 +34,9 @@ function buildCreateSlice({ creators } = {}) {
     // 定义方法
     const contextMethods = {
       addCase(typeOrActionCreator, reducer2) {
+        // typeOrActionCreator是reducer的标识，reducer2是函数
+        // 在示例中，typeOrActionCreator是"test/increment"
+
         const type =
           typeof typeOrActionCreator === "string"
             ? typeOrActionCreator
@@ -53,7 +56,10 @@ function buildCreateSlice({ creators } = {}) {
                 type
           );
         }
+
+        // 把函数保存到（此切片内部）上下文的sliceCaseReducersByType里面
         context.sliceCaseReducersByType[type] = reducer2;
+        // 继续返回方法，可以链式调用
         return contextMethods;
       },
       addMatcher(matcher, reducer2) {
@@ -64,16 +70,19 @@ function buildCreateSlice({ creators } = {}) {
         return contextMethods;
       },
       exposeAction(name2, actionCreator) {
+        // 把actionCreator的方法存起来！
         context.actionCreators[name2] = actionCreator;
         return contextMethods;
       },
       exposeCaseReducer(name2, reducer2) {
+        // name2的入参是"increment"
+        // 把函数保存到（此切片内部）上下文的sliceCaseReducersByName里面
         context.sliceCaseReducersByName[name2] = reducer2;
         return contextMethods;
       },
     };
 
-    // 执行每一个reducer（初始化执行）
+    // 保存每一个reducer（初始化）
     const reducers =
       (typeof options.reducers === "function"
         ? options.reducers(buildReducerCreators())
@@ -100,7 +109,8 @@ function buildCreateSlice({ creators } = {}) {
           cAT
         );
       } else {
-        // 否则就是一个正常的reducer函数
+        // 如果是一个正常的reducer函数就走下面
+        // 目的是保存reducer函数、和对应的action创建函数（一个reducer一个action）
         handleNormalReducerDefinition(
           reducerDetails,
           reducerDefinition,
@@ -146,8 +156,8 @@ function buildCreateSlice({ creators } = {}) {
       });
     }
     const selectSelf = (state) => state;
-    const injectedSelectorCache = /* @__PURE__ */ new Map();
-    const injectedStateCache = /* @__PURE__ */ new WeakMap();
+    const injectedSelectorCache = new Map();
+    const injectedStateCache = new WeakMap();
     let _reducer;
     function reducer(state, action) {
       if (!_reducer) _reducer = buildReducer();
@@ -212,13 +222,15 @@ function buildCreateSlice({ creators } = {}) {
         selectSlice,
       };
     }
+
+    // 返回一个方法汇总工具包
     const slice = {
       name,
-      reducer,
-      actions: context.actionCreators,
-      caseReducers: context.sliceCaseReducersByName,
+      reducer, // reducer的执行方法
+      actions: context.actionCreators, // action创建函数的集合对象
+      caseReducers: context.sliceCaseReducersByName, // 真实的reducer函数对象
       getInitialState,
-      ...makeSelectorProps(reducerPath),
+      ...makeSelectorProps(reducerPath), // selector相关方法
       injectInto(injectable, { reducerPath: pathOpt, ...config } = {}) {
         const newReducerPath = pathOpt ?? reducerPath;
         injectable.inject(
@@ -251,8 +263,15 @@ function handleNormalReducerDefinition(
   maybeReducerWithPrepare,
   context
 ) {
+  // 入参：
+  // 第一个入参是reducer函数的一些信息（type是唯一的reducer函数的标识，reducerName是reducer函数的名字，createNotation是true）
+  // 第二个入参是reducer函数
+  // 第三个入参是上下文方法合集
+
   let caseReducer;
   let prepareCallback;
+
+  //
   if ("reducer" in maybeReducerWithPrepare) {
     if (
       createNotation &&
@@ -267,8 +286,11 @@ function handleNormalReducerDefinition(
     caseReducer = maybeReducerWithPrepare.reducer;
     prepareCallback = maybeReducerWithPrepare.prepare;
   } else {
+    // 普通的走下面，创建一个中间变量保存reducer函数
     caseReducer = maybeReducerWithPrepare;
   }
+
+  // 执行
   context
     .addCase(type, caseReducer)
     .exposeCaseReducer(reducerName, caseReducer)
@@ -276,4 +298,37 @@ function handleNormalReducerDefinition(
       reducerName,
       prepareCallback ? createAction(type, prepareCallback) : createAction(type)
     );
+}
+
+function createAction(type, prepareAction) {
+  // type是reducer的唯一标识
+  // prepareAction是reducer的prepare方法
+  function actionCreator(...args) {
+    if (prepareAction) {
+      let prepared = prepareAction(...args);
+      if (!prepared) {
+        throw new Error(false ? 0 : "prepareAction did not return an object");
+      }
+      return {
+        type,
+        payload: prepared.payload,
+        ...("meta" in prepared && {
+          meta: prepared.meta,
+        }),
+        ...("error" in prepared && {
+          error: prepared.error,
+        }),
+      };
+    }
+    return {
+      type,
+      payload: args[0],
+    };
+  }
+  actionCreator.toString = () => `${type}`;
+  actionCreator.type = type;
+  actionCreator.match = (action) =>
+    (0, redux__WEBPACK_IMPORTED_MODULE_0__.isAction)(action) &&
+    action.type === type;
+  return actionCreator;
 }
